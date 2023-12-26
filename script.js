3df3332dcbb84cb3de6ae1e14d8cde00f7443e97
ZephaniahQ <zeph.qamar4@gmail.com>
2023-12-26 18:22:34 +0500

    finalized arduino blog

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var categories = document.getElementsByClassName('selection-box');
+    
+    // Adding a click listener to each category element
+    for (var i = 0; i < categories.length; i++) {
+        categories[i].addEventListener('click', function() {
+            // Retrieve the data-href attribute and redirect to the URL
+            var url = this.getAttribute('data-href');
+            window.location.href = url;
+        });
+    }
+});
